fa271662c970186741987f7cd36870a397dffccf
dhruvgusai <dhruvgusai7600@gmail.com>
2025-01-10 19:00:26 +0530

    done rendering

--- RenderingList/src/App.jsx
+++ RenderingList/src/App.jsx
@@ -2,13 +2,27 @@ import List from './List.jsx'
 
 function App() {
 
-const fruits=[{key:1, name:"apple", calories:95},
-              {key:2, name:"banana", calories:45},
-              {key:3, name:"orange", calories:125},
-              {key:4, name:"pineapple", calories:37},
-              {key:5, name:"coconut", calories:155}];
+const fruits=[{id:1, name:"Apple", calories:95},
+              {id:2, name:"Banana", calories:45},
+              {id:3, name:"Orange", calories:125},
+              {id:4, name:"Pineapple", calories:37},
+              {id:5, name:"Coconut", calories:155}];
 
-  return (<List item={fruits} category="Fruits" />);
+
+const vegetables=[{id:6, name:"Potatoes", calories:95},
+                  {id:7, name:"Celery", calories:45},
+                  {id:8, name:"Carrots", calories:125},
+                  {id:9, name:"Corn", calories:37},
+                  {id:10, name:"Broccoli", calories:155}];
+  
+
+  return (<>
+            {fruits.length>0 && <List item={fruits} category="Fruits" /> }  {/* its is way to use ternary operator in short way */}
+            {vegetables.length>0 ? <List item={vegetables} category="Vegetables" /> : null }
+            
+          </>
+  
+);
 }
 
 export default App
--- RenderingList/src/List.jsx
+++ RenderingList/src/List.jsx
@@ -1,9 +1,9 @@
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 
 function List(props){
 
     const category = props.category;
-    const itemList= props.items;
+    const itemList= props.item;
 
    // fruits.sort((a,b)=> a.name.localeCompare(b.name)); // ALPHABETIC 
    //fruits.sort((b,a)=> a.name.localeCompare(b.name));  // REVESE ALPHABETIC
@@ -12,15 +12,28 @@ function List(props){
 
    //const lowCalFruits= fruits.filter(fruit=> fruit.calories< 100);
     
-    const listItems=itemList.map(item => <li key={item.id}> 
-                                                 {item.name}
-                                            : <b>{item.calories}</b></li>
-    );
+    const listItems = itemList.map(item => (<li key={item.id}> 
+                                                   {item.name}
+                                            : <b>  {item.calories}</b></li>
+    ));
 
     return( <>
-                <h3>{category}</h3>
-                <ol>{listItems}</ol>
-    </> );
+                <h3 className="list-category">{category}</h3>
+                <ol className="list-items">{listItems}</ol>
+    </> 
+    );
+}
+
+List.PropTypes={
+    category: PropTypes.string,
+    item: PropTypes.arrayOf(PropTypes.shape({id: PropTypes.number,
+                                             name: PropTypes.string,
+                                             calories: PropTypes.number}))
+}
+
+List.defaultProps= { 
+    category: "Category",
+    items: []
 }
 
 export default List;
